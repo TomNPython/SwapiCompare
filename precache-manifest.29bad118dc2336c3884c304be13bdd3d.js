63f75200a3cf71cca57d9bec4de42fed3842d38e
tomni <tom.nicholson1000@gmail.com>
2019-12-17 10:32:18 +0700

    Updates

---
+++ precache-manifest.29bad118dc2336c3884c304be13bdd3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3ac05c7f5b1d16e9080f53d2ef0864f",
+    "url": "/SwapiCompare/index.html"
+  },
+  {
+    "revision": "5761bd7a0e44f1c54876",
+    "url": "/SwapiCompare/static/css/main.5abd67af.chunk.css"
+  },
+  {
+    "revision": "350f99ac6ac0103fd908",
+    "url": "/SwapiCompare/static/js/2.6b44af2c.chunk.js"
+  },
+  {
+    "revision": "5761bd7a0e44f1c54876",
+    "url": "/SwapiCompare/static/js/main.8cd8215b.chunk.js"
+  },
+  {
+    "revision": "489d63bb88c8ad2ca6ae",
+    "url": "/SwapiCompare/static/js/runtime-main.f29cdf27.js"
+  },
+  {
+    "revision": "64ffe439606a92d8a0fee194e92c6c3f",
+    "url": "/SwapiCompare/static/media/Spin.64ffe439.svg"
+  },
+  {
+    "revision": "773e9c96302d513491ca03bf33dd558a",
+    "url": "/SwapiCompare/static/media/stars.773e9c96.jpg"
+  }
+]);
